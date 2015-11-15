3498569241dd5b9735f674151ad149d7e9bd845c
Nick <npalenchar@gmail.com>
2015-11-14 20:39:56 -0500

    finished Tag class

--- script.js
+++ script.js
@@ -16,8 +16,21 @@
   children: [child1Ref, child2Ref, childNRef]
 }
 */
-
-function parseTag(str){
+var Tag = function(tagName, body, isBlock, attributes){
+  this.tagName = tagName;
+  this.body = body;
+  this.isBlock = isBlock;
+  if(!attributes) this.attributes = {};
+  else this.attributes = attributes;
+  if(isBlock) this.children = [];
+  else this.children = null;
+}
+Tag.prototype.addChild = function(child) {
+  if(this.children !== null) this.children.push(child);
+  else throw new Error("Cannot add children to inline objects (set isBlock to 'true')");
+}
+var HTMLParser = function () {};
+HTMLParser.prototype.parse = function (str){
   var tags = []
   str = str.split(/[<|>]/).forEach(function(tag) {
     if(!tag)return;
--- testspec.js
+++ testspec.js
@@ -1,9 +1,9 @@
 describe("The Tag class", function(){
   it("should be instantiated with a tagName, body, block, and attribute class", function(){
     var newTag = new Tag('div', 'body text', true)
-    expect(newTag.tagName).toEqual('p');
+    expect(newTag.tagName).toEqual('div');
     expect(newTag.body).toEqual('body text');
-    expect(newTag.hasOwnProperty(isBlock)).toEqual(true);
+    expect(newTag.hasOwnProperty('isBlock')).toEqual(true);
     expect(newTag.attributes).toEqual({});
   });
   it("can have additional properties in the attributes object", function(){
@@ -11,18 +11,35 @@ describe("The Tag class", function(){
     expect(newTag.attributes.src).toEqual('#');
     expect(newTag.attributes.href).toEqual('localhost');
   });
+  describe("The children attribute",function(){
+    var parent = new Tag('div', 'parent', true);
+    var child1 = new Tag('div', 'child 1', true);
+    var child2 = new Tag('div', 'child 2', true);
+    var inline = new Tag('p', 'paragraph', false);
+    it("block elements should have a children attribute as an empty array", function(){
+      expect(parent.children).toEqual([]);
+      expect(inline.children).toBeNull();
+      });
+    it("has an addChild method",function(){
+      parent.addChild(child1);
+      parent.addChild(child2);
+      expect(typeof parent.addChild).toEqual('function');
+      expect(parent.children.length).toEqual(2);
+    });
+  });
 });
 
 xdescribe("The html tag parser", function() {
+  var parser = new HTMLParser();
   it("returns an epty array if there's nothing to pares", function(){
     expect(parseTag('')).toEqual([]);
   });
   it("gets the correct tag name", function(){
-    expect(parseTag('<span></span>')[0].tag).toEqual('span');
-    expect(parseTag('<p></p>')[0].tag).toEqual('p');
+    expect(parser.parse('<span></span>')[0].tag).toEqual('span');
+    expect(parser.parse('<p></p>')[0].tag).toEqual('p');
      });
   it("can parse multiple tags", function(){
-    expect(parseTag('<span></span><p></p><h1></h1>').map(function(tagObj){
+    expect(parparser.parseser.parse('<span></span><p></p><h1></h1>').map(function(tagObj){
       return tagObj.tag;
     })).toEqual(['span','p','h1'])
     });
@@ -33,9 +50,9 @@ xdescribe("Parsing attributes & content", function() {
   var pTagLong ="<p>This tag has way too much text. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et venenatis risus, nec dignissim justo. Integer molestie pellentesque bibendum. Nullam congue posuere efficitur. Praesent eget accumsan ex. Quisque nulla mauris, accumsan sed consequat a, condimentum ac augue. Suspendisse potenti. Aliquam malesuada consectetur est, vitae vestibulum libero vehicula id. Sed suscipit, quam eu dignissim dictum, velit justo eleifend erat, eu convallis dolor lectus a eros. Nunc et mauris lectus. Nulla laoreet pretium venenatis. Maecenas id blandit turpis.</p>";
   var divTag = "<div id='divId' class='divClass'>Here's some content</div>";
   
-  var parsedDivTag = parseTag(divTag)[0];
-  var parsedPTag = parseTag(pTag)[0];
-  var parsedPTagLong = parseTag(pTagLong)[0];
+  var parsedDivTag = parser.parse(divTag)[0];
+  var parsedPTag = parser.parse(pTag)[0];
+  var parsedPTagLong = parser.parse(pTagLong)[0];
   
   it("adds a content attribute which is an object",function(){
     expect(typeof parsedDivTag.content).toEqual('object');
